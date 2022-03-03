159f2c17ca1b77ceb7d808db7f6808973ba32572
quabenaa <quabenaa@gmail.com>
2022-03-03 09:17:16 +0000

    feat: add jwt token to register and login

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const UserService = require('./UserService');
 const UserValidation = require('./UserValidation');
 const router = express.Router();
-
+const jwtSecret = 'coding-challenge';
 router.post(
   '/api/register',
   UserValidation.email,
@@ -10,8 +11,15 @@ router.post(
   UserValidation.userExists,
   async (req, res) => {
     await UserService.save(req);
-    return res.status(201).send({ token: '_testToken' });
+    const token = jwt.sign({ email: req.body.email }, jwtSecret);
+    return res.status(201).send({ token });
   }
 );
+
+router.post('/api/login', UserValidation.email, UserValidation.password, async (req, res) => {
+  await UserService.save(req);
+  const token = jwt.sign({ email: req.body.email }, jwtSecret);
+  return res.status(201).send({ token });
+});
 
 module.exports = router;
--- src/user/UserService.js
+++ src/user/UserService.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const save = async (req) => {
   const hashPassword = await bcrypt.hash(req.body.password, 10);
   const user = { ...req.body, password: hashPassword };
-  await req.db.collection('users').insertOne(user);
+  return await req.db.collection('users').insertOne(user);
 };
 
 module.exports = { save };
